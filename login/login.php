bb566ffc498187ca2eafee34ba0bd6480a2e8392
mrishason <mrishasons@gmail.com>
2021-05-05 14:00:05 -0700

    database connection and login form script

---
+++ login/login.php
@@ -0,0 +1,39 @@
+<?php
+error_reporting(E_ALL & ~ E_NOTICE);
+session_start();
+ $pagetitle="LogIn Page";
+?>
+<?php
+       if ($_POST['submit']){
+        include 'connection.php';
+        $username=($_POST['username']);
+        $password=($_POST['password']);
+
+        $sql="SELECT id, username, password,utype FROM users WHERE username='$username' AND password='$password'";
+        $query=mysqli_query($dbcon, $sql);
+        if($query){
+          $row= mysqli_fetch_row($query);
+          $userId= $row[0];
+          $dbusername=$row[1];
+          $dbpassword=$row[2];
+		  $utype=$row[3];
+        }
+           if($username== $dbusername && $password== $dbpassword && $utype=='Admin'){
+          $_SESSION['username']=$username;
+          $_SESSION['id']=$userId;
+          header('Location:adminDashbored.php');
+        }
+		else if($username== $dbusername && $password== $dbpassword && $utype=='user'){
+          $_SESSION['username']=$username;
+          $_SESSION['id']=$userId;
+          header('Location:userDashbored.php');
+		}
+		
+		
+		else{
+            echo "<span style='color:red;'>User name or password is incorrect!</span>";
+          }    
+	   }
+?>
+
+     
